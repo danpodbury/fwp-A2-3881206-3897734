f131dfe5527e88f90593298321c63fb903727b5a
Oliver G <82480796+TheOliG@users.noreply.github.com>
2022-10-15 18:03:09 +1100

    Feature/connecting post backend (#18)
    * Trying to connect post backend
    
    * useeffect
    
    * Posting and getting posts now works
    
    Co-authored-by: Oliver <“s3897734@student.rmit.edu.au”>
    Co-authored-by: Dan Podbury <29648303+danpodbury@users.noreply.github.com>

--- frontend/src/models/Post.js
+++ frontend/src/models/Post.js
@@ -1,7 +1,7 @@
 class Post{
-    constructor(user, body, timestamp, parent=null) {
+    constructor(user, body, timestamp=null, parent=null) {
       this.post_id = -1;
-      this.user_id = user.id;
+      this.user_id = user.user_id;
       this.name = user.name;
       this.body = body;
       this.timestamp = timestamp;
