44def4d7e521881c58bc475c903a44342cea90d1
Dan Podbury <29648303+danpodbury@users.noreply.github.com>
2022-10-18 16:55:43 +1100

    update mockserver

--- frontend/src/models/Reaction.js
+++ frontend/src/models/Reaction.js
@@ -1,6 +1,6 @@
 class Reaction{
     constructor({post_id, user, type}) {
-      this.id = null;
+      this.reaction_id = null;
       this.post_id = post_id;
       this.user_id = user.user_id;
       this.type = type; // the reaction type, encoded as an int
