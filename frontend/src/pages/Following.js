1974460d91169f6af50acb6b8206adc728d5ba79
Oliver G <82480796+TheOliG@users.noreply.github.com>
2022-10-19 21:32:22 +1100

    Feature/follwing feed (#27)
    * Doesnt work but almost does
    
    * Is functional now (aside from delete)
    
    * Finished unfollow functionality
    
    Co-authored-by: Oliver <“s3897734@student.rmit.edu.au”>

--- frontend/src/pages/Following.js
+++ frontend/src/pages/Following.js
@@ -2,8 +2,11 @@ import '../App.css';
 import User from '../models/User';
 import { NavLink } from "react-router-dom";
 import "./Following.css";
-import { useState } from "react"
+import { useState, useEffect } from 'react';
 import { ConfirmationModal } from '../components/Modal.js';
+import * as FollowerRepo from '../repository/Follow';
+import * as UserRepo from '../repository/User';
+
 
 //For testing purposes, will need to properly implement later
 //TODO: connect to backend
@@ -15,16 +18,37 @@ tempFollowingUsers.push(new User(103,"Alyssa","","","2/1/2019"));
 tempFollowingUsers.push(new User(104,"Bruce","","","4/12/2021"));
 
 function Following() {
-
-    const [followingUsers, setFollowingUsers] = useState(tempFollowingUsers);
+  const [followingUsers, setFollowingUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
     
     const unFollowUser = (id) =>{
-      setFollowingUsers(followingUsers.filter(item => item.getId() !== parseInt(id))); 
+      FollowerRepo.removeFollow(JSON.parse(localStorage.getItem("currentUser")).user_id, id);
+      setFollowingUsers(followingUsers.filter(item => item.user_id !== parseInt(id))); 
+    }
+
+    //Renders the following users
+    useEffect(() => {
+      getFolloweeUserList();
+    }, []);
+
+    async function getFolloweeUserList(){
+      var currentUser = JSON.parse(localStorage.getItem("currentUser"));
+      var tempListOfUsers = [];
+      var result = await FollowerRepo.getFollowees(currentUser.user_id)
+      console.log(result);
+      for(var i=0; i<result.length; i++){
+        const userObj = await UserRepo.getUserById(result[i]["publisher_id"]);
+        console.log("User obj: "+userObj);
+        tempListOfUsers.push(userObj);
+      }
+      console.log(tempListOfUsers);
+      setFollowingUsers(tempListOfUsers);
+      setLoading(false);
     }
     
 
     return (
-      <div className='App'>
+      <div>
         <table className="table table-hover rightaAlignTableData">
           <thead>
             <tr>
@@ -32,16 +56,16 @@ function Following() {
             </tr>
           </thead>
           <tbody>
-        {followingUsers.length > 0 ? 
+        {!loading ? 
         followingUsers.map((user)=>(
-            <tr key={user.getId()}>
-              <th scope="row"><NavLink to={`/user/${user.getId()}`} className="btn btn-lg btn-primary">{user.getName()}</NavLink></th>
-              <td><ConfirmationModal onConfirm={()=>unFollowUser(user.getId())} buttonText="Unfollow User" body={`Are you sure you want to unfollow ${user.getName()}?`} confirmText="Unfollow"/></td>
+            <tr key={"following"+user.user_id}>
+              <th scope="row"><NavLink to={`/user/${user.user_id}`} className="btn btn-lg btn-primary">{user.name}</NavLink></th>
+              <td><ConfirmationModal onConfirm={()=>unFollowUser(user.user_id)} buttonText="Unfollow User" body={`Are you sure you want to unfollow ${user.name}?`} confirmText="Unfollow"/></td>
             </tr>
         ))
     :
             <tr>
-              <th style={{textAlign: "center"}}>You are not currently following anyone!</th>
+              <th style={{textAlign: "center"}}>Loading...</th>
             </tr>
     }
           </tbody>
